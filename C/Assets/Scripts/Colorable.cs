378f7d816796c60911754072cf5e7de19f7e75f6
mclee3360 <mclee3360@gmail.com>
2017-03-02 23:37:20 -0500

    Change alpha color tint to be grayish and slightly transparent

--- C/Assets/Scripts/Colorable.cs
+++ C/Assets/Scripts/Colorable.cs
@@ -15,6 +15,9 @@ public abstract class Colorable : Moveable {
         } else if (gameObject.layer == 10)  {
             // Blue tint
             sr.color = new Color(0F, 0F, 0.7F);
+        } else if (gameObject.layer == 11) {
+            // Alpha layer
+            sr.color = new Color(0.9F, 0.9F, 0.9F, 0.5F);
         } else {
             // Normal Colors
             sr.color = new Color(1F, 1F, 1f);
