0cb737562fe19885aa2ed790360d97a04256ea6b
Thor Keller <thorboomafoo@gmail.com>
2017-03-05 16:09:33 -0500

    Added 2 new enemies

---
+++ C/Assets/Scripts/FollowingEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingEnemy : EnemyInk
+{
+    public bool pauses = false;
+    public int radius = 5;
+    public int layer = 8;
+    public void Start()
+    {
+        this.gameObject.layer = layer;
+    }
+    public void Update()
+    {
+        float dist = Time.deltaTime*movementSpeed*2;
+        GameObject player = player = GameObject.FindGameObjectWithTag("Player");
+        float px = player.transform.position.x;
+        float py = player.transform.position.y;
+        float dy = py - transform.position.y;
+        float dx = px - transform.position.x;
+        float total = (float)System.Math.Sqrt(dy * dy + dx * dx);
+        //todo: check if player in the layer
+        if (total < radius && this.gameObject.layer == 0 &&
+            (!pauses || (((PlayerMovement)player.GetComponent("PlayerMovement")).getCurrentDirection()==(dx>=0))))
+        {
+            dy = dy *dist / total;
+            dx = dx * dist / total;
+            transform.position = new Vector3(transform.position.x + dx, transform.position.y + dy, 0);
+        }
+    }
+}
---
+++ C/Assets/Scripts/LayerShiftingEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerShiftingEnemy : EnemyInk
+{
+    public float time = 5;
+    public int initialLayer = 8;
+    private float temptime = 0;
+
+    public void Start()
+    {
+        this.gameObject.layer = initialLayer;
+    }
+    public void Update()
+    {
+        temptime += Time.deltaTime;
+        if (temptime > time)
+        {
+            temptime = 0;
+            this.gameObject.layer = ((this.gameObject.layer - 7) % 4) + 8;
+        }
+    }
+}
