d038000ea60f88aecbe69f53f1624b549b26773f
mclee3360 <mclee3360@gmail.com>
2017-03-08 21:03:06 -0500

    Add sliding functionality on "icy" platforms

--- C/Assets/Scripts/Icey.cs
+++ C/Assets/Scripts/Icey.cs
@@ -3,5 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Icey : Moveable {
-    //do not use
+
+    // When player touches an icy platform.
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            PlayerMovement moveCtrl = other.gameObject
+                .GetComponent(typeof(PlayerMovement)) as PlayerMovement;
+            moveCtrl.setSlide(true);
+        }
+    }
+
+    // When player is no longer touching an icy platform
+    void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            PlayerMovement moveCtrl = other.gameObject
+                .GetComponent(typeof(PlayerMovement)) as PlayerMovement;
+            moveCtrl.setSlide(false);
+        }
+    }
 }
