bf7c4499d05329cbdaf1d8832cbeae27c87f1b71
Emily Chen <chenemily3@yahoo.com>
2017-04-01 01:40:44 -0400

    Fix menu, trampoline, set up cutscenes

---
+++ C/Assets/Scripts/Non Entities/LevelButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelButton : MonoBehaviour
+{
+    public int sceneNum = 1;
+
+    void OnMouseEnter()
+    {
+        gameObject.transform.localScale += new Vector3(0.1f, 0, 0);
+    }
+
+    void OnMouseExit()
+    {
+        gameObject.transform.localScale -= new Vector3(0.1f, 0, 0);
+    }
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+
+    /*public void PlayClicked()
+    {
+        AudioSource audio = gameObject.GetComponent<AudioSource>();
+        audio.Play();
+        SceneManager.LoadScene(1);
+    }*/
+}
