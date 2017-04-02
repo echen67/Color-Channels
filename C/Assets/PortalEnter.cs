fc5a42c5039cc639e5d4060e2dff218d3dd4004b
Emily Chen <chenemily3@yahoo.com>
2017-04-01 23:33:09 -0400

    Portal

---
+++ C/Assets/PortalEnter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalEnter : MonoBehaviour {
+
+    public GameObject portalExit;
+    
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Player")
+        {
+            coll.transform.position = portalExit.transform.position;
+        }
+    }
+}
