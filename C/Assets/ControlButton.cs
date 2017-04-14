f87b6202d8473b4949d3b8b7175773d164a59b43
Emily Chen <chenemily3@yahoo.com>
2017-04-14 15:24:50 -0400

    Pause, Controls, Credits

---
+++ C/Assets/ControlButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlButton : MonoBehaviour {
+
+    public GameObject controlPanel;
+
+    public void ControlClicked()
+    {
+        controlPanel.SetActive(true);
+        AudioSource audio = gameObject.GetComponent<AudioSource>();
+        audio.Play();
+    }
+}
